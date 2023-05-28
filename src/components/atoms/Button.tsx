a3a472ffb76fa06baaf39a840fd916f656919d3f
Andrew 'AJ' Jones <aj@darkpeak.dev>
2023-05-28 21:01:38 +0100

    Adds vitest and prettier

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -24,26 +24,21 @@ interface ButtonProps {
 }
 
 const sizeMap = {
-  'small': 'w-24 h-8',
-  'medium': 'w-32 h-10',
-  'large': 'w-40 h-12',
-}
+  small: 'w-24 h-8',
+  medium: 'w-32 h-10',
+  large: 'w-40 h-12'
+};
 
 const typeMap = {
-  'primary': 'text-white bg-purple-600 hover:bg-purple-400',
-  'secondary': 'text-black bg-gray-200 hover:bg-gray-100',
-  'tertiary': 'text-white bg-blue-600 hover:bg-blue-400',
-}
+  primary: 'text-white bg-purple-600 hover:bg-purple-400',
+  secondary: 'text-black bg-gray-200 hover:bg-gray-100',
+  tertiary: 'text-white bg-blue-600 hover:bg-blue-400'
+};
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  type = 'primary',
-  size = 'medium',
-  label,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ type = 'primary', size = 'medium', label, ...props }: ButtonProps) => {
   return (
     <button
       type="button"
